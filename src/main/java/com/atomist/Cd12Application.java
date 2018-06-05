32643838fbb3d499f4b01f3541ad24e858856b93
Christian Dupuis <cd@atomist.com>
2018-06-05 16:05:13 +0200

    Update Cd12Application.java

--- src/main/java/com/atomist/Cd12Application.java
+++ src/main/java/com/atomist/Cd12Application.java
@@ -23,7 +23,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Cd12Application {
 
 	public static void main(String[] args) {
-		int test = 0 / 1;
+		int test = 1 / 0;
 		SpringApplication.run(Cd12Application.class, args);
 	}
 }
